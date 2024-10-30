c9453fbdf37c13a5c803b0a18194e662ff23747a
K-suk <kuma.kou.0201@gmail.com>
2024-10-30 13:49:36 -0700

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 import crypto from 'crypto';
 
+const nonce = crypto.randomBytes(16).toString('base64');
+
 export default {
   reactStrictMode: true,
   images: {
@@ -18,18 +20,14 @@ export default {
           { key: 'Permissions-Policy', value: 'geolocation=(self), microphone=()' },
           {
             key: 'Content-Security-Policy',
-            value: (req, res) => {
-              const nonce = crypto.randomBytes(16).toString('base64');
-              res.setHeader('Content-Security-Policy', `
-                default-src 'self';
-                script-src 'self' 'nonce-${nonce}' https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://vercel.live;
-                style-src 'self' 'nonce-${nonce}' https://trusted-cdn.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com;
-                img-src 'self' data: https://mdbcdn.b-cdn.net;
-                connect-src 'self' https://ubcbuddies.onrender.com;
-                frame-ancestors 'none';
-              `.replace(/\s{2,}/g, ' ').trim());
-              return res;
-            },
+            value: `
+              default-src 'self';
+              script-src 'self' 'nonce-${nonce}' https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://vercel.live;
+              style-src 'self' 'nonce-${nonce}' https://trusted-cdn.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com;
+              img-src 'self' data: https://mdbcdn.b-cdn.net;
+              connect-src 'self' https://ubcbuddies.onrender.com;
+              frame-ancestors 'none';
+            `.replace(/\s{2,}/g, ' ').trim(),
           },
           { key: 'Cache-Control', value: 'public, max-age=31536000, immutable' },
         ],
