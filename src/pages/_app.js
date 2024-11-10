6ca0f76f30278c4906ac01594b244001e16f5667
K-suk <kuma.kou.0201@gmail.com>
2024-11-10 14:30:23 -0800

    try to fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,57 +2,48 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import Navbar from '../../components/Navbar';
+import Navbar from '../components/Navbar';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import Script from 'next/script';
 import App from 'next/app';
-import { NonceContext } from '../../context/NonceContext';
-
-import '../../public/assets/css/style.css';
+import { NonceContext } from '../context/NonceContext';
 
 config.autoAddCss = false;
 
-function MyApp({ Component, pageProps }) {
-    const router = useRouter();
-    const noNavbarPaths = ['/login', '/signup', '/password-reset', '/activate/[uid]/[token]', '/password/reset/confirm/[uid]/[token]'];
-    const nonce = pageProps.nonce || '';  // pagePropsからnonceを取得
-    console.log('Nonce passed to NonceContext.Provider in _app.js:', nonce);  // nonceが取得できているか確認
+function MyApp({ Component, pageProps, nonce }) {
+  const router = useRouter();
+  const noNavbarPaths = ['/login', '/signup', '/password-reset', '/activate/[uid]/[token]', '/password/reset/confirm/[uid]/[token]'];
+  console.log('Nonce passed to NonceContext.Provider in _app.js:', nonce);
 
-    useEffect(() => {
-        if (typeof window !== 'undefined') {
-            require('bootstrap/dist/js/bootstrap.bundle.min.js');
-        }
-    }, []);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      require('bootstrap/dist/js/bootstrap.bundle.min.js');
+    }
+  }, []);
 
-    return (
-        <NonceContext.Provider value={nonce}>
-            {!noNavbarPaths.includes(router.pathname) && <Navbar />}
-            <Component {...pageProps} />
-
-            {/* Scriptタグにnonceを追加 */}
-            <Script
-                src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"
-                strategy="beforeInteractive"
-                nonce={nonce}
-            />
-            <Script
-                src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
-                strategy="beforeInteractive"
-                nonce={nonce}
-            />
-        </NonceContext.Provider>
-    );
+  return (
+    <NonceContext.Provider value={nonce}>
+      {!noNavbarPaths.includes(router.pathname) && <Navbar />}
+      <Component {...pageProps} />
+      <Script
+        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/2.11.6/umd/popper.min.js"
+        strategy="beforeInteractive"
+        nonce={nonce}
+      />
+      <Script
+        src="https://stackpath.bootstrapcdn.com/bootstrap/5.1.3/js/bootstrap.bundle.min.js"
+        strategy="beforeInteractive"
+        nonce={nonce}
+      />
+    </NonceContext.Provider>
+  );
 }
 
-// getInitialPropsでnonceをpagePropsに追加
 MyApp.getInitialProps = async (appContext) => {
-    const appProps = await App.getInitialProps(appContext);
-    const nonce = appContext.ctx.req ? appContext.ctx.req.nonce : 'nein!';
-    console.log(appContext.ctx);
-    console.log('Nonce received in _app.js from _document.js:', nonce);
-
-    return { ...appProps, pageProps: { ...appProps.pageProps, nonce } };
+  const appProps = await App.getInitialProps(appContext);
+  const nonce = appContext.ctx.req ? appContext.ctx.req.nonce : '';
+  return { ...appProps, pageProps: { ...appProps.pageProps, nonce } };
 };
 
 export default MyApp;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -7,26 +7,23 @@ class MyDocument extends Document {
     const initialProps = await Document.getInitialProps(ctx);
     const nonce = crypto.randomBytes(16).toString('base64');
     console.log('[Server] Generated nonce in _document.js:', nonce);
-    
-    // nonceをpageProps経由で直接渡すために設定
-    ctx.req.nonce = nonce;  // ctx.reqオブジェクトに追加
     return { ...initialProps, nonce };
   }
 
   render() {
-    const { nonce } = this.props.nonce;
-    console.log('[Server] Rendering with nonce in _document.js:', nonce);
-
-    // Content-Security-Policyの設定内容を変数として保持
-    const csp = `default-src 'self'; script-src 'self' 'nonce-${nonce}' https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://vercel.live; style-src 'self' 'nonce-${nonce}' https://trusted-cdn.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com; img-src 'self' data: https://mdbcdn.b-cdn.net; connect-src 'self' https://ubcbuddies.onrender.com;`;
+    const { nonce } = this.props;
+    const csp = `
+      default-src 'self';
+      script-src 'self' 'nonce-${nonce}' https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com https://vercel.live;
+      style-src 'self' 'nonce-${nonce}' https://trusted-cdn.com https://cdnjs.cloudflare.com https://stackpath.bootstrapcdn.com;
+      img-src 'self' data: https://mdbcdn.b-cdn.net;
+      connect-src 'self' https://ubcbuddies.onrender.com;
+    `.replace(/\s{2,}/g, ' ').trim();
 
     return (
       <Html>
-        <Head nonce={nonce}>
-          <meta
-            httpEquiv="Content-Security-Policy"
-            content={csp}  // 変数cspを渡す
-          />
+        <Head>
+          <meta httpEquiv="Content-Security-Policy" content={csp} />
         </Head>
         <body>
           <Main />
